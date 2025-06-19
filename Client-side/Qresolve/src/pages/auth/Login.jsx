ffd32629cb81809a7df3c239bfba7ab01af6fe4a
MuhammedAfsalCP <muhammedafsalcp8@gmail.com>
2025-06-19 12:20:52 +0530

    requriments added

---
+++ Client-side/Qresolve/src/pages/auth/Login.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import bg_white from '../../assets/login-white.png'
+export const Login = () => {
+  return (
+    <div className="bg-cover bg-center h-[100vh] w-[100vw]"
+      style={{ backgroundImage: `url(${bg_white})` }}></div>
+  )
+}
