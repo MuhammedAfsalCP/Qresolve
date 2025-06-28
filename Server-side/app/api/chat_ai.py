ee2b268981070687f5dfc3f16ee8b1d978d5168d
MuhammedAfsalCP <muhammedafsalcp8@gmail.com>
2025-06-28 10:28:34 +0530

    ai

--- Server-side/app/api/chat_ai.py
+++ Server-side/app/api/chat_ai.py
@@ -1,11 +1,26 @@
-# app/api/chat.py
-from fastapi import APIRouter, WebSocket, Depends, HTTPException, status
+from fastapi import APIRouter, WebSocket, status
 from app.services.websocket_manager import manager
 from app.services.gemini_chat import get_gemini_response
-from app.auth.dependencies import get_current_user
-from app.models.token import TokenData
-from app.auth.decode_token import decode_jwt_token
-
 
 router = APIRouter()
 
+@router.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+
+    user_id = "anon-user"
+    await manager.connect(websocket, user_id)
+
+    try:
+        await manager.send_personal_message("👋 Hello! I'm Gemini AI. Ask me anything.", websocket)
+
+        while True:
+            user_input = await websocket.receive_text()
+            print(f"📨 {user_id}: {user_input}")
+
+            ai_response = get_gemini_response(user_input)
+            await manager.send_personal_message(ai_response, websocket)
+
+    except Exception as e:
+        print(f"❗ Error with {user_id}: {e}")
+    finally:
+        manager.disconnect(websocket)
