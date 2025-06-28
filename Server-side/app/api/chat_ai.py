249ff8151021f54efc34f16cc6b61df1bde05431
MuhammedAfsalCP <muhammedafsalcp8@gmail.com>
2025-06-28 10:11:30 +0530

    conflict

--- Server-side/app/api/chat_ai.py
+++ Server-side/app/api/chat_ai.py
@@ -9,29 +9,3 @@ from app.auth.decode_token import decode_jwt_token
 
 router = APIRouter()
 
-@router.websocket("/ws/{client_id}")
-async def websocket_endpoint(websocket: WebSocket,user: dict = Depends(get_current_user) ):
-    
-    if not user:
-        await websocket.close(code=status.WS_1008_POLICY_VIOLATION)
-        print(f"User Not logined")
-        return
-
-
-    await manager.connect(websocket, user.user_id)
-
-    try:
-        await manager.send_personal_message("Hello! How can I help you today?", websocket)
-
-        while True:
-            user_input = await websocket.receive_text()
-            print(f"📨 {user.user_id} ({client_id}): {user_input}")
-
-            ai_response = get_gemini_response(user_input)
-
-            await manager.send_personal_message(ai_response, websocket)
-
-    except Exception as e:
-        print(f"Error with {user.user_id}: {e}")
-    finally:
-        manager.disconnect(websocket)
